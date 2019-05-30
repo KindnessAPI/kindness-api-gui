a31af48d63606cfb7242ad4c0a173967f6d5f5b1
Wong Lok <yellowhappy831@gmail.com>
2019-05-31 06:03:58 +0800

    front-end-deploy

--- src/ka/api.js
+++ src/ka/api.js
@@ -1,5 +1,6 @@
 export const load = ({ pageAt, perPage } = { perPage: 10, pageAt: 0 }) => {
-  return fetch(`/www.wonglok.com/quotes/?as=json&offset=${pageAt * perPage}&limit=${perPage}`, {
+  return fetch(`https://kindnessapi.com/www.wonglok.com/quotes/?as=json&offset=${pageAt * perPage}&limit=${perPage}`, {
+    mode: 'cors'
   })
     .then(r => {
       if (r.ok) {
