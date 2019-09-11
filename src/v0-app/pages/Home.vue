2a2f681e64474451c18ff1ce5b2f7f6fb72e17c2
wonglok <yellowhappy831@gmail.com>
2019-09-11 11:51:35 +0800

    front-end-deploy

--- src/v0-app/pages/Home.vue
+++ src/v0-app/pages/Home.vue
@@ -23,7 +23,7 @@
         </div>
       </div>
       <div class="hidden sm:hidden lg:block lg:w-1/2">
-        <div class="lg:relative h-full d-max-w-4xl">
+        <div class="lg:relative h-full ddmax-w-4xl">
           <img class="absolute inset-0 h-full w-full object-cover object-center" src="./img/galaxy.jpg" alt="Woman workcationing on the beach">
         </div>
       </div>
