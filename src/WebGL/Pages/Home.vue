21654b94dd2823e1dcfce58faf9d5612e80351bb
wonglok <yellowhappy831@gmail.com>
2020-04-05 09:08:46 +0800

    git backup

--- src/WebGL/Pages/Home.vue
+++ src/WebGL/Pages/Home.vue
@@ -2,7 +2,15 @@
   <div class="full">
     <div class="fixed top-0 left-0 full" style="z-index: -1" ref="mounter"></div>
     <!-- <HappyLayout v-if="ready" @scene="scene = $event" @camera="camera = $event"></HappyLayout> -->
-    <div class="w-full">
+    <div>
+      <div class=" h-64"></div>
+      <ScissorArea>
+        <div slot="dom" class="p-3 text-green-500 text-4xl">
+          KindnessAPI.com
+        </div>
+        <HomeBannerLayout slot="o3d"></HomeBannerLayout>
+      </ScissorArea>
+      <div class=" h-64"></div>
       <ScissorArea>
         <div slot="dom" class="p-3 text-green-500 text-4xl">
           KindnessAPI.com
@@ -17,13 +25,6 @@
         <HomeBannerLayout slot="o3d"></HomeBannerLayout>
       </ScissorArea>
       <div class=" h-64"></div>
-      <div class=" h-64"></div>
-      <ScissorArea>
-        <div slot="dom" class="p-3 text-green-500 text-4xl">
-          KindnessAPI.com
-        </div>
-        <HomeBannerLayout slot="o3d"></HomeBannerLayout>
-      </ScissorArea>
     </div>
   </div>
 </template>
