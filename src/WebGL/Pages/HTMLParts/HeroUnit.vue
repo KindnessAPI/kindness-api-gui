63e464cdeb6c8b0fd19e07171aacd37b1489aac6
wonglok <yellowhappy831@gmail.com>
2020-04-07 06:06:26 +0800

    git backup

--- src/WebGL/Pages/HTMLParts/HeroUnit.vue
+++ src/WebGL/Pages/HTMLParts/HeroUnit.vue
@@ -4,11 +4,11 @@
       <div class="px-3 pt-3 pb-1 text-3xl md:text-4xl font-bold">
         Let's Connect People with Prayers & Kindness.
       </div>
-      <div class="px-3">
-        <span class="font-bold text-red-600 bg-red-200 p-1">Assure</span> love, <span class="font-bold text-green-600 bg-green-200 p-1">Pray</span> for each other and <span class="font-bold text-blue-600 bg-blue-200 p-1">Inform</span> Kindness.
+      <div class="px-3 mb-1">
+        <span class="font-bold text-red-600 bg-red-200 p-1">Assure</span> love, <span class="font-bold text-green-600 bg-green-200 p-1">Pray</span> for each other & <span class="font-bold text-blue-600 bg-blue-200 p-1">Inform</span> Kindness.
       </div>
       <div class="px-3">
-        Spreading the word of ✞ Gospel via our Global Technology Infrastructure
+        Spreading the word of ✞ Gospel via our Global Technology Infrastructure and Computer Graphics Art.
       </div>
 
       <!--
@@ -17,6 +17,7 @@
         </div>
       -->
     </div>
+
     <ScissorArea>
       <div slot="dom" class="full">
         <div class="h-min90 md:h-min70">
