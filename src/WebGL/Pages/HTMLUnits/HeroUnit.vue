3a96504b285bec8780802f7b4ac88ab0d6f19218
wonglok <yellowhappy831@gmail.com>
2020-04-10 16:07:41 +0800

    git backup

--- src/WebGL/Pages/HTMLUnits/HeroUnit.vue
+++ src/WebGL/Pages/HTMLUnits/HeroUnit.vue
@@ -5,14 +5,14 @@
         Making Kindness Louder at Scale
       </div>
       <div class="px-3 text-lg mb-2">
-        At this age, a lot of people are depressed and grieving because they lost their loved ones to Co-VID-19 or they lost their job or business. People need kindness and love from each other, and encouargement from Christ community as well.
+        At such an unprecedented and unfamiliar time as we are going through today, many people are not only dealing with depression and anxiety, but also grieving the loss of loved ones to Co-VID-19. Moreover, many have lost their jobs or businesses. Now, more than ever, people need love and kindness from each other, and encouragement from The Christ community as well.
       </div>
       <div class="px-3 mb-2 text-3xl md:text-4xl font-bold">
         Why do we make KindessAPI, when there's social media?
       </div>
       <div class="px-3 text-lg mb-2">
-        It's easy to encourage others on social media, but the amount of audience is quite low due to attention fatigue and the excessive amount of content in social media feed.
-        API is a good way to spread kindness because it doesn't have ads or unrelated content and the Kindness content can be integrated to other websites or apps.
+        It is easy to encourage others on social media, but the audience limited, due to attention fatigue due to the bombardment of negative content on the social media feed.
+        API is a good way to spread kindness because it doesn't have ads or unrelated content, and also because the Kindness content can be integrated into other websites or apps.
       </div>
     </div>
 
@@ -29,10 +29,10 @@
         If you want to encourage others...
       </div>
       <div class="px-3 text-lg mb-2">
-        We can help you distirubte your essential message through our system to different places such as different websites, platforms or apps which integrated with KindnessAPI.
+        We can help you distribute your essential message through our system to different platforms such as websites, or apps which integrated with KindnessAPI.
       </div>
       <div class="px-3 text-lg mb-2">
-        We can also help you organise tons of micro-content with hashtags or folders.
+        We can also help you organize tons of micro-content using hashtags or folders.
       </div>
     </div>
     <div class="pb-3">
@@ -40,7 +40,7 @@
         If you want to Spread the Kindness to a wider audience....
       </div>
       <div class="px-3 text-lg mb-2">
-        You can help out humanity by embeding a Kindness Essential Message Box into your website, so that we have a wider reach of audience of Kindness and Bible.
+        You can help out humanity by embedding a Kindness Essential Message Box into your website so that you have a wider reach of audience to Kindness and contents from the Bible.
       </div>
     </div>
     <div class="pb-3">
@@ -48,7 +48,7 @@
         If you're a developer...
       </div>
       <div class="px-3 text-lg mb-2">
-        If you're a developer, you can use our simple REST API to integrate or query kindness quotes into your App or Website Platform.
+        If you are a developer, you can use our simple REST API to integrate or query kindness quotes and bible verses into your App or Website Platform.
       </div>
     </div>
 
@@ -64,5 +64,5 @@ export default {
 }
 </script>
 
-<style>
+<style lang="postcss">
 </style>
