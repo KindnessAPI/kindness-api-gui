663a69a95ac910cc46dadb2f34c13bcef8a2d0c2
wonglok <yellowhappy831@gmail.com>
2020-04-12 09:25:02 +0800

    git backup

--- src/WebGL/Pages/HTMLUnits/TopNavBar.vue
+++ src/WebGL/Pages/HTMLUnits/TopNavBar.vue
@@ -9,9 +9,12 @@
         </router-link>
         <div class="mr-6 flex cursor-pointer">
           <div class="hidden md:inline-flex">
-            <div class="text-2xl text-black p-3">
-                Love Work in Progress
-            </div>
+            <!-- <div class="text-2xl text-black p-3" @click="$router.push('/dash')">
+              Love Work in Progress
+            </div> -->
+            <router-link v-if="!Auth.isLoggedIn" exact-active-class="underline" class="text-xl text-black py-2 pr-3" to="/login">Login</router-link>
+            <router-link v-if="!Auth.isLoggedIn" exact-active-class="underline" class="text-xl text-black py-2 pr-3" to="/register">Register</router-link>
+            <router-link v-if="Auth.isLoggedIn" exact-active-class="underline" class="text-xl text-black py-2 pr-3" to="/dashboard">Dashboard</router-link>
           </div>
           <img @click="$emit('menu')" src="../icon/menu.svg" alt="">
         </div>
@@ -23,9 +26,16 @@
 </template>
 
 <script>
+import { Auth } from '../../../APIs/KA'
 export default {
   components: {
     ...require('../../webgl').default
+  },
+  data () {
+    return {
+      Auth,
+      Store: Auth.Store
+    }
   }
 }
 </script>
