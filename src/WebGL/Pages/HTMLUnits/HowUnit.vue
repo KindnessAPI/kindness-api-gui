33efc52e3a438b5e33cf1e01dfaa0acd1139adf1
wonglok <yellowhappy831@gmail.com>
2020-04-25 21:17:33 +0800

    git backup

--- src/WebGL/Pages/HTMLUnits/HowUnit.vue
+++ src/WebGL/Pages/HTMLUnits/HowUnit.vue
@@ -4,7 +4,7 @@
 
         <div class="flex flex-col lg:flex-row">
 
-          <div class="lg:w-1/2 relative border-t lg:border-r border-black bgimg">
+          <div class="lg:w-1/2 relative border-t border-black bgimg">
             <ScissorArea class="lg:absolute top-0 left-0 right-0 bottom-0 h-min100 lg:h-auto" style="z-index: -1;">
               <div slot="dom" class="full">
               </div>
