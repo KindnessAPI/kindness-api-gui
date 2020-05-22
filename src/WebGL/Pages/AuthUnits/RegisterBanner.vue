c72d650be3d729b763d68d4f05c15b5558ad6655
wonglok <yellowhappy831@gmail.com>
2020-05-22 10:10:40 +0800

    git backup

--- src/WebGL/Pages/AuthUnits/RegisterBanner.vue
+++ src/WebGL/Pages/AuthUnits/RegisterBanner.vue
@@ -1,24 +1,27 @@
 <template>
-  <div class="stickybanner p-4 flex justify-center border-gray-400 border-t">
-    <div class="flex justify-between items-center w-full" v-if="!me">
-      <div class="w-1/5">
+  <div class="">
+    <div class="taller"></div>
+    <div class="fixedbanner p-4 flex justify-center border-gray-400 border-t">
+      <div class="flex justify-between items-center w-full" v-if="!me">
+        <div class="w-1/5">
+        </div>
+        <div class="w-3/5 flex justify-center">
+          <router-link to="/login" class="inline-block rounded-full px-5 py-2 m-2 bg-blue-300">Login</router-link>
+          <router-link to="/register" class="inline-block rounded-full px-5 py-2 m-2 bg-blue-600 text-white">Register</router-link>
+        </div>
+        <div class="w-1/5 flex justify-end">
+          <img src="./icon/close.svg" @click="close" class="cursor-pointer" alt="">
+        </div>
       </div>
-      <div class="w-3/5 flex justify-center">
-        <router-link to="/login" class="inline-block rounded-full px-5 py-2 m-2 bg-blue-300">Login</router-link>
-        <router-link to="/register" class="inline-block rounded-full px-5 py-2 m-2 bg-blue-600 text-white">Register</router-link>
-      </div>
-      <div class="w-1/5 flex justify-end">
-        <img src="./icon/close.svg" @click="close" class="cursor-pointer" alt="">
-      </div>
-    </div>
-    <div class="flex justify-between items-center w-full" v-else-if="me">
-      <div class="w-1/5">
-      </div>
-      <div class="w-3/5 flex justify-center">
-        <router-link to="/galaxy" class="inline-block rounded-full px-5 py-2 m-2 bg-blue-600 text-white text-center">Welcome Back! @{{ me.username }}</router-link>
-      </div>
-      <div class="w-1/5 flex justify-end">
-        <img src="./icon/close.svg" @click="close" class="cursor-pointer" alt="">
+      <div class="flex justify-between items-center w-full" v-else-if="me">
+        <div class="w-1/5">
+        </div>
+        <div class="w-3/5 flex justify-center">
+          <router-link to="/galaxy" class="inline-block rounded-full px-5 py-2 m-2 bg-blue-600 text-white text-center">Welcome Back! @{{ me.username }}</router-link>
+        </div>
+        <div class="w-1/5 flex justify-end">
+          <img src="./icon/close.svg" @click="close" class="cursor-pointer" alt="">
+        </div>
       </div>
     </div>
   </div>
@@ -40,13 +43,16 @@ export default {
 }
 </script>
 
-<style scoped>
-.stickybanner{
-  position: sticky;
+<style scoped lang="postcss">
+.fixedbanner{
+  position: fixed;
   bottom: 0px;
   left: 0px;
+  height: 89px;
   width: 100%;
   background-color: rgba(255,255,255,0.95);
-  -webkit-backdrop-filter: blur(20px);
+}
+.taller{
+  height: 89px;
 }
 </style>
--- src/WebGL/Pages/HTMLUnits/MakeFriendsUnit.vue
+++ src/WebGL/Pages/HTMLUnits/MakeFriendsUnit.vue
@@ -16,7 +16,7 @@
           </TraverseDemoUnit>
         </div>
 
-        <div class="block lg:hidden absolute top-0 left-0 right-0 bottom-0"></div>
+        <!-- <div class="block lg:hidden absolute top-0 left-0 right-0 bottom-0"></div> -->
         <!--  -->
       </div>
       <div class="lg:w-1/2 relative">
